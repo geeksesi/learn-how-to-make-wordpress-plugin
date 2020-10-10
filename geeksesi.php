001e0092ce2a5264a36ba0d3b87c2ef6ce8b0417
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2020-10-10 11:25:55 +0330

    just make plugin

---
+++ geeksesi.php
@@ -0,0 +1,8 @@
+/*
+Plugin Name: geeksesi
+Plugin URI: http://github.com/geeksesi/learn-how-to-make-wordpress-plugin
+Description: this plugins maded for learning purpose.
+Author: Mohammad Javad Ghasemy
+Version: 1.0.0
+Author URI: https://geeksesi.ir
+*/
